b33986c5fd1d3f7e7e8cf62aaeef3cf93345095d
spl3g <spleefer6@yandex.ru>
2024-04-13 02:51:40 +0300

    add some packages
    add some packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,5 +2,6 @@
   tgs2png = pkgs.callPackage ./tgs2png { };
   pokemon-colorscripts = pkgs.callPackage ./pokemon-colorscripts { };
   tdlib = pkgs.callPackage ./tdlib { };
-  amnezia-client = pkgs.qt6Packages.callPackage ./amnezia-client { };
+  spmp = pkgs.callPackage ./spmp { };
+  spmp-server = pkgs.callPackage ./spmp-server { };
 }
