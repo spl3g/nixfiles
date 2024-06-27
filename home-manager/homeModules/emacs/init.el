d7d2c0f04c867fca1fec90967193209dd5c24a1b
spl3g <spleefer6@yandex.ru>
2024-06-27 17:40:41 +0300

    emacs stuff

--- home-manager/homeModules/emacs/init.el
+++ home-manager/homeModules/emacs/init.el
@@ -10,7 +10,10 @@
  '(auth-source-save-behavior nil)
  '(custom-safe-themes
    '("d77d6ba33442dd3121b44e20af28f1fae8eeda413b2c3d3b9f1315fbda021992" "80214de566132bf2c844b9dee3ec0599f65c5a1f2d6ff21a2c8309e6e70f9242" default))
- '(package-selected-packages '(marginalia embark-consult)))
+ '(eshell-modules-list
+   '(eshell-alias eshell-banner eshell-basic eshell-cmpl eshell-dirs eshell-tramp eshell-extpipe eshell-glob eshell-hist eshell-ls eshell-pred eshell-prompt eshell-script eshell-term eshell-unix))
+ '(package-selected-packages '(marginalia embark-consult))
+ '(safe-local-variable-values '((engine . go))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -18,3 +21,4 @@
  ;; If there is more than one, they won't work right.
  )
 (put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
