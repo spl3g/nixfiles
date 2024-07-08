3aa0f29d570d5073857788fe41dd2b02970cd14d
spl3g <spleefer6@yandex.ru>
2024-07-08 11:44:04 +0500

    add scanner config

--- nixos/nixosModules/printing.nix
+++ nixos/nixosModules/printing.nix
@@ -5,12 +5,16 @@
     printing.enable = lib.mkEnableOption "enable printing";
   };
   config = lib.mkIf config.printing.enable {
+    hardware.sane = {
+      enable = true;
+      extraBackends = [ pkgs.hplipWithPlugin ];
+    };
     services = {
       printing.enable = true;
       printing.drivers = [ pkgs.hplipWithPlugin ];
       avahi = {
         enable = true;
-        nssmdns = true;
+        nssmdns4 = true;
         openFirewall = true;
       };
     };
