98782e6dc44e1b6df38e2bb6d669aa43b0a374cc
spl3g <spleefer6@yandex.ru>
2024-11-19 23:50:41 +0300

    feat: introduce vt = next to try to fix the boot logs in my greeter

--- nixos/nixosModules/greetd.nix
+++ nixos/nixosModules/greetd.nix
@@ -11,6 +11,9 @@
     services.greetd = {
       enable = true;
       settings = {
+        terminal = {
+          vt = "next";
+        };
         default_session = {
           command = "${pkgs.greetd.tuigreet}/bin/tuigreet -t -r --asterisks --user-menu -c ${config.greetd.command}";
           user = "jerpo";
