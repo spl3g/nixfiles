59236b8501647e3712aaec606a8ed27ce7fcbaa1
spl3g <spleefer6@yandex.ru>
2024-12-27 01:01:06 +0300

    feat: use colors from stylix in dunst

--- home-manager/homeModules/dunst.nix
+++ home-manager/homeModules/dunst.nix
@@ -7,16 +7,12 @@
   config = lib.mkIf config.dunst.enable {
     home.packages = [ pkgs.libnotify ];
     services.dunst = {
-      enable = false;
+      enable = true;
       settings = {
         global = {
           origin = "bottom-right";
-          frame_color = "#c4a7e7";
           notification_limit = 5;
           progress_bar = true;
-          background = "#232136";
-          foreground = "#e0def4";
-          separator_color = "foreground";
         };
       };
     };
