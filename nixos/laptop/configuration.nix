9d9429ad10ce989557a96d07d72617b0e0f83d10
spl3g <spleefer6@yandex.ru>
2025-04-13 00:05:27 +0300

    feat: add power management config

--- nixos/laptop/configuration.nix
+++ nixos/laptop/configuration.nix
@@ -1,4 +1,4 @@
-{ inputs, outputs, pkgs, ... }: {
+{ pkgs, ... }: {
   # You can import other NixOS modules here
   imports = [
     # If you want to use modules your own flake exports (from modules/nixos):
@@ -20,20 +20,35 @@
   docker.enable = true;
   greetd.command = "Hyprland";
 
+  powerManagement.enable = true;
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 20;
+
+      USB_AUTOSUSPEND = 0;
+    };
+  };
+
   time.timeZone = "Europe/Moscow";
   networking.hostName = "ltrr-mini";
-
-  virtualisation.libvirtd.enable = true;
-  programs.virt-manager.enable = true;
+  networking.hosts = {
+    "127.0.0.1" = [ "mr.local" "local.oneln.ru" ];
+    "127.0.0.3" = [ "local-api.oneln.ru" ];
+  };
 
   programs.adb.enable = true;
   services.udev.packages = [
     pkgs.android-udev-rules
-  ];
-
-  services.joycond.enable = true;
-  environment.systemPackages = with pkgs; [
-    joycond-cemuhook
   ];
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
