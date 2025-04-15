ba1f63c6c5137dc9a7c8550ff2f1f052377a0091
spl3g <spleefer6@yandex.ru>
2025-04-15 22:44:04 +0300

    feat(hyprland): cleanup scripts

--- home-manager/homeModules/attachments/hypr-scripts/switch-sink.py
+++ home-manager/homeModules/attachments/hypr-scripts/switch-sink.py
@@ -5,7 +5,6 @@ import subprocess
 status = subprocess.run(["wpctl", "status"], stdout=subprocess.PIPE).stdout.decode('utf-8')
 status_lines = status.split('\n')
 sink_line = 0
-selected_sink = 0
 sinks = []
 sink_names = []
 
@@ -25,17 +24,26 @@ for line in status_lines[sink_line:]:
     end_name = line[1].index('[')
     sink = nums[8:]
     if nums[4] == '*':
-        selected_sink = sink
+        continue
         
     sinks.append(sink)
     sink_names.append(name[:end_name].strip())
 
-next_sink = sinks.index(selected_sink) + 1
-if next_sink == len(sinks):
-    next_sink = 0
+if (len(sinks) == 0):
+    subprocess.run(["notify-send", "Pipewire sink", "No sinks to switch to"])
+    exit(0)
+
+selected_sink = subprocess.run(["rofi", "-dmenu"], input="\n".join(sink_names), capture_output=True, text=True).stdout[:-1]
+
+if len(selected_sink) == 0:
+    exit(0)
+
+try:
+    next_sink = sink_names.index(selected_sink)
+except Exception:
+    subprocess.run(["notify-send", "Pipewire sink", "Sink not found"])
+    exit(1)
     
-next_sink_name = sink_names[next_sink]
 subprocess.run(["wpctl", "set-default", sinks[next_sink]])
 
-subprocess.run(["notify-send", "Pipewire sink", "Selected " + next_sink_name])
-
+subprocess.run(["notify-send", "Pipewire sink", "Selected " + selected_sink])
