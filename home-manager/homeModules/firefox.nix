7da94ff3fe3e6678cada89b0fccbd4a3294d1804
spl3g <spleefer6@yandex.ru>
2025-05-01 15:06:51 +0300

    fix(firefox): replace iconUpdateURL with icon

--- home-manager/homeModules/firefox.nix
+++ home-manager/homeModules/firefox.nix
@@ -56,43 +56,43 @@
                 }
               ];
               
-              iconUpdateURL = "https://cdn.search.brave.com/serp/v2/_app/immutable/assets/safari-pinned-tab.539899c7.svg";
+              icon = "https://cdn.search.brave.com/serp/v2/_app/immutable/assets/safari-pinned-tab.539899c7.svg";
               updateInterval = 24 * 60 * 60 * 1000;
               definedAliases = [ "!br" ];
             };
             "NixOS Packages" = {
               urls = [{ template = "https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query={searchTerms}"; }];
-              iconUpdateURL = "https://nixos.org/favicon.png";
+              icon = "https://nixos.org/favicon.png";
               updateInterval = 24 * 60 * 60 * 1000;
               definedAliases = [ "!ns" ];
             };
             "NixOS Options" = {
               urls = [{ template = "https://search.nixos.org/options?channel=unstable&from=0&size=50&sort=relevance&type=packages&query={searchTerms}"; }];
-              iconUpdateURL = "https://nixos.org/favicon.png";
+              icon = "https://nixos.org/favicon.png";
               updateInterval = 24 * 60 * 60 * 1000;
               definedAliases = [ "!no" ];
             };
             "HomeManager" = {
               urls = [{ template = "https://home-manager-options.extranix.com/?query={searchTerms}&release=master"; }];
-              iconUpdateURL = "https://github.com/mipmip/home-manager-option-search/blob/main/images/favicon.png";
+              icon = "https://github.com/mipmip/home-manager-option-search/blob/main/images/favicon.png";
               updateInterval = 24 * 60 * 60 * 1000;
               definedAliases = [ "!hs" ];
             };
             "NixWiki" = {
               urls = [{ template = "https://wiki.nixos.org/w/index.php?search={searchTerms}"; }];
-              iconUpdateURL = "https://nixos.org/favicon.png";
+              icon = "https://nixos.org/favicon.png";
               updateInterval = 24 * 60 * 60 * 1000;
               definedAliases = [ "!nw" ];
             };
             "Kinopoisk" = {
               urls = [{ template = "https://www.kinopoisk.ru/index.php?kp_query={searchTerms}"; }];
-              iconUpdateURL = "https://www.kinopoisk.ru/favicon.ico";
+              icon = "https://www.kinopoisk.ru/favicon.ico";
               updateInterval = 24 * 60 * 60 * 1000;
               definedAliases = [ "!ks" ];
             };
             "MDN Docs" = {
               urls = [{ template = "https://developer.mozilla.org/en-US/search?q={searchTerms}"; }];
-              iconUpdateURL = "https://developer.mozilla.org/favicon-48x48.bc390275e955dacb2e65.png";
+              icon = "https://developer.mozilla.org/favicon-48x48.bc390275e955dacb2e65.png";
               updateInterval = 24 * 60 * 60 * 1000;
               definedAliases = [ "!md" ];
             };
