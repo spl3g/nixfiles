f7746d1482f499257dafc87be27154832edf3781
spl3g <spleefer6@yandex.ru>
2025-08-12 00:53:16 +0500

    feat: remove tdlib

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,7 +1,6 @@
 { pkgs ? import <nixpkgs> { } }: {
   tgs2png = pkgs.callPackage ./tgs2png { };
   pokemon-colorscripts = pkgs.callPackage ./pokemon-colorscripts { };
-  tdlib = pkgs.callPackage ./tdlib { };
   spmp = pkgs.callPackage ./spmp { };
   spmp-server = pkgs.callPackage ./spmp-server { };
   hydralauncher = pkgs.callPackage ./hydralauncher { };
