f476b1054bd41fc6046cf2c96ab880b0c54824aa
spl3g <spleefer6@yandex.ru>
2025-08-12 00:52:12 +0500

    fix(mako): move variables into settings

--- home-manager/homeModules/mako.nix
+++ home-manager/homeModules/mako.nix
@@ -8,14 +8,12 @@
   config = lib.mkIf config.mako.enable {
     services.mako = {
       enable = true;
-      anchor = "bottom-right";
-      # backgroundColor = "#1f1d2e";
-      # borderColor = "#e0def4";
-      # textColor = "#e0def4";
-      defaultTimeout = 5000;
-      borderSize = 3;
-      borderRadius = 7;
-      # font = "Rubik 11";
+      settings = {
+        anchor = "bottom-right";
+        defaultTimeout = 5000;
+        borderSize = 3;
+        borderRadius = 7;
+      };
     };
   };
 }
