5772551755e22c8c138dcb72f2900a82423a3384
spl3g <spleefer6@yandex.ru>
2025-08-12 00:52:56 +0500

    feat(hyprland): improve the tg-toggle script

--- home-manager/homeModules/attachments/hypr-scripts/toggle-tg.sh
+++ home-manager/homeModules/attachments/hypr-scripts/toggle-tg.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
+TG_CLASS=org.telegram.desktop
 
-tg_workspace=$(hyprctl clients -j | jq -e '.[] | select((.class | contains("org.telegram.desktop"))) | .workspace.id')
+tg_workspace=$(hyprctl clients -j | jq -e ".[] | select((.class | contains(\"${TG_CLASS}\"))) | .workspace.id")
 if [[ -z $tg_workspace ]]; then
 	telegram-desktop
 elif [[ $tg_workspace -eq $(hyprctl activeworkspace -j | jq -e '.id') ]]; then
-	hyprctl dispatch pin class:org.telegram.desktop
-	hyprctl dispatch movetoworkspacesilent special:magic,class:org.telegram.desktop
+	hyprctl dispatch pin class:$TG_CLASS
+	hyprctl dispatch movetoworkspacesilent special:magic,class:$TG_CLASS
 else
-	hyprctl dispatch movetoworkspacesilent +0,class:org.telegram.desktop
-	hyprctl dispatch pin class:org.telegram.desktop
+	hyprctl dispatch movetoworkspacesilent +0,class:$TG_CLASS
+	hyprctl dispatch pin class:$TG_CLASS
+	hyprctl dispatch focuswindow
 fi
