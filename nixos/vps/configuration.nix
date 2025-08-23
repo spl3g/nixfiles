d11e33bc968e556c38d43a3cc35a52980d8024b8
spl3g <spleefer6@yandex.ru>
2025-08-24 00:36:48 +0500

    feat(vps): cleanup unneded things

--- nixos/vps/configuration.nix
+++ nixos/vps/configuration.nix
@@ -18,16 +18,7 @@
     80
     443 
     # xray
-    57625 
-    # wireguard
-    53
-    5201
-  ];
-  
-  networking.firewall.allowedUDPPorts = [
-    # wireguard
-    53 
-    51820
+    57625
   ];
 
   networking.domain = "kcu.su";
@@ -53,41 +44,10 @@
     "147.45.40.6" = ["stereotyped-sheet.aeza.network" "stereotyped-sheet"];
   };
 
-  networking.nat = {
-    enable = true;
-    externalInterface = "ens3";
-    internalInterfaces = [ "wg0" ];
-  };
-  
-  networking.wg-quick = {
-    interfaces.wg0 = {
-      address = [ "10.1.1.1/32" ];
-      listenPort = 51820;
-
-      privateKeyFile = "/root/wireguard-keys/private";
-
-      preUp = "sysctl -w net.ipv4.ip_forward=1";
-
-      peers = [
-        {
-          allowedIPs = [ "10.1.1.2/32" ];
-          publicKey = "kzXzxJu1AdcRI5UwtGOrN6WuTZYqJo++PYRrXdOu/lY=";
-          persistentKeepalive = 25;
-        }
-      ];
-    };
-  };
-
-  # services.headscale = {
-  #   enable = true;
-  #   port = 8123;
-  # };
-  
   services.openssh = {
     enable = true;
     settings.PasswordAuthentication = false;
   };
-  
   
   environment.systemPackages = map lib.lowPrio [
     pkgs.curl
@@ -98,12 +58,6 @@
     root = {
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDJ8UW1BXDGDmlaiARO3a9boTG8wknUyITMz0Z0OJpHx spleefer6@yandex.ru"
-      ];
-    };
-    fimoz = {
-      isNormalUser = true;
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ//SNTK/qZmoT0YH7tHxXg6VGEbxbxPLXQVC1zsBPb4 90ts@mail.ru"
       ];
     };
   };
@@ -129,27 +83,6 @@
         ";
         recommendedProxySettings = false;
       };
-
-      "chat".proxyPass = "http://127.0.0.1:30017";
-      "argocd".proxyPass = "https://127.0.0.1:30018";
-      "health".proxyPass = "http://127.0.0.1:8090";
-      "music".proxyPass = "http://127.0.0.1:4747";
-    };
-
-    extraVirtualHosts = {
-      "_" = {
-        locations."/" = {
-          return = 444;
-        };
-      };
-    };
-
-    home =
-      let
-        homeConfig = import ../server/configuration.nix { inherit modulesPath config lib pkgs; };
-      in {
-        subdomains = homeConfig.nginx.subdomains;
-        url = "http://10.1.1.2";
     };
   };
 
@@ -162,50 +95,6 @@
         "/root/x-ui:/etc/x-ui"
       ];
     };
-  };
-
-  services.k3s = {
-    enable = false;
-    role = "server";
-    extraFlags = ["--disable traefik"];
-  };
-
-  gonic = {
-    enable = true;
-    extraGroups = ["music" "files"];
-    musicPaths = ["/srv/files/music"];
-  };
-
-  boot.supportedFilesystems = [ "nfs" ];  
-  services.nfs.idmapd = {
-    settings = {
-      General = {
-        Domain = "kcu.su";
-      };
-      Mapping = {
-        Nobody-User = "nobody";
-        Nobody-Group = "nogroup";
-      };
-    };
-  };
-  filesDir = {
-    enable = true;
-    subPaths = [
-      {
-        path = "music";
-        group = "music";
-      }
-    ];
-  };
-  fileSystems."/srv/files/music" = {
-    device = "10.1.1.2:/music";
-    fsType = "nfs";
-    options = [
-      "defaults"
-      "x-systemd.automount"
-      "x-systemd.mount-timeout=30"
-      "_netdev"
-    ];
   };
 
   system.stateVersion = "24.05";
