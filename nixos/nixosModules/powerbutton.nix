58f992a0ffa9c35c9c49b56a9e2f65489f180079
spl3g <spleefer6@yandex.ru>
2025-10-27 23:47:17 +0300

    fix(powerButton): update with the new options

--- nixos/nixosModules/powerbutton.nix
+++ nixos/nixosModules/powerbutton.nix
@@ -5,10 +5,10 @@
     pbutton.disable = lib.mkEnableOption "enable pbutton";
   };
   config = lib.mkIf config.pbutton.disable {
-    services.logind.extraConfig = ''
-      HandlePowerKey=ignore
-      HandleLidSwitch=suspend
-      HandleLidSwitchExternalPower=suspend
-    '';
+    services.logind.settings.Login = {
+      HandlePowerKey = "ignore";
+      HandleLidSwitch = "suspend";
+      HandleLidSwitchExternalPower = "suspend";
+    };
   };
 }
